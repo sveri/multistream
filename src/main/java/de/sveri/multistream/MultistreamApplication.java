599cba30a3b96bd8ec7c4e52f6c2ccfe80a6fb19
sveri <sveri80@gmail.com>
2025-06-18 09:18:52 +0200

    initial commit

---
+++ src/main/java/de/sveri/multistream/MultistreamApplication.java
@@ -0,0 +1,13 @@
+package de.sveri.multistream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultistreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultistreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/sveri/multistream/MultistreamApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sveri.multistream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultistreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
